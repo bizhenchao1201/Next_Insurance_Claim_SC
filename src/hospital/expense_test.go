4467420809f467c156d4d9f5983688093b170c1a
zhenchao.bi <zhenchao.bi@accenture.com>
2017-07-10 23:44:02 +0800

    add

--- src/hospital/expense_test.go
+++ src/hospital/expense_test.go
@@ -30,6 +30,36 @@ func TestHospitalChainCode_Invoke(t *testing.T) {
 	}
 
 	fmt.Printf("Query payload is %s", string(queryRes.Payload))
+}
+
+func TestHospitalChainCode_Claim(t *testing.T) {
+
+	scc := new(HospitalChainCode)
+	stub := shim.NewMockStub("test2", scc)
+
+	jsonVal := `{"uid":"3702821982","expenseTime":"20001010010203","claimed":false,"medicines":[{"name":"med1000","id":"1000","number":10,"price":10},{"name":"med2000","id":"2000","number":10,"price":20},{"name":"med3000","id":"3000","number":10,"price":30}]}`
+	res := stub.MockInvoke("1", [][]byte{[]byte("invoke"), []byte(jsonVal)})
+	if res.Status != shim.OK {
+		t.FailNow()
+	}
+
+	jsonVal2 := `{"uid":"3702821982","expenseTime":"20001010010204","claimed":false,"medicines":[{"name":"med1000","id":"1000","number":10,"price":10},{"name":"med2000","id":"2000","number":10,"price":20},{"name":"med3000","id":"3000","number":10,"price":30}]}`
+	res2 := stub.MockInvoke("1", [][]byte{[]byte("invoke"), []byte(jsonVal2)})
+	if res2.Status != shim.OK {
+		t.FailNow()
+	}
+
+	res3 := stub.MockInvoke("1", [][]byte{[]byte("claim"), []byte("3702821982"), []byte("20001010010204")})
+	if res3.Status != shim.OK {
+		t.FailNow()
+	}
+
+	queryRes := stub.MockInvoke("2", [][]byte{[]byte("query"), []byte("3702821982")})
+	if queryRes.Status != shim.OK {
+		t.FailNow()
+	} else {
+		fmt.Printf("Query payload is %s", string(queryRes.Payload))
+	}
 
 
 }
